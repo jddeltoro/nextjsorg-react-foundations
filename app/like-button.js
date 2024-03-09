a7a1aabdf8c829e6230de04ede958522c87c900d
jddeltoro <jdiazdeltoro@gmail.com>
2024-03-08 23:58:52 -0500

    update comment

--- app/like-button.js
+++ app/like-button.js
@@ -3,6 +3,9 @@ import { useState } from 'react';
 export default function LikeButton() {
     const [likes, setLikes] = useState(0);
     
+    /**
+     * Increments the like count when the button is clicked.
+     */
     function handleClick() {        
         setLikes(likes + 1);
     }
