f129ce8ad80dc1a6e5d2aad7491ddeaa3c97f349
jddeltoro <jdiazdeltoro@gmail.com>
2024-03-10 23:54:42 -0400

    add animation on click

--- app/like-button.js
+++ app/like-button.js
@@ -8,6 +8,18 @@ export default function LikeButton() {
      */
     function handleClick() {        
         setLikes(likes + 1);
+        animateButton();
+    }
+
+    /**
+     * Animates the button when it is clicked.
+     */
+    function animateButton() {
+        const button = document.querySelector('button');
+        button.classList.add('animate');
+        setTimeout(() => {
+            button.classList.remove('animate');
+        }, 500);
     }
 
     return (
